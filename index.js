318651cf2cc5787f4f001de8409e870e3a486dd4
yasir7341 <yasirabdulrahman75@gmail.com>
2022-08-16 23:26:15 +0300

    Completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+describe('index.js', function () {
+    describe('companyName', function () {
+      it('is set as Scuber', function () {
+        expect(companyName).to.equal('Scuber');
+      });
+      
